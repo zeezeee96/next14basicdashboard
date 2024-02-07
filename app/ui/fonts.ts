b24c1e103a1591bcbdeda43870210419e9f16534
Muhammad Aziz <engr.muhammadaziz96@gmail.com>
2024-02-07 15:34:11 +0500

    added initial dashboard

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter } from 'next/font/google';
+import { Poppins } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const poppins = Poppins({ weight: ['400', '700'], subsets: ['latin'] });
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
